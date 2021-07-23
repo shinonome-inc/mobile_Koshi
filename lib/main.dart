122e352d568e4cc96d7dd458c68d7c89f4e51d6d
Koshi Yoshida <k.yoshida@4nonome.com>
2021-07-23 13:36:57 +0900

    fixed code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_qiita_application/top_page.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      initialRoute: '/first',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/first': (context) => TopPage(),
+      },
       theme: ThemeData(
         // This is the theme of your application.
         //
