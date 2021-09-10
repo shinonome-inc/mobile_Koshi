87373df7e2176317da3b243547c42efcc5a1bbd2
Koshi Yoshida <k.yoshida@4nonome.com>
2021-09-10 19:10:41 +0900

    デザインを変えました

--- lib/feed_error_page.dart
+++ lib/feed_error_page.dart
@@ -12,10 +12,10 @@ class FeedErrorPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 180.67),
+            Expanded(child: Container()),
             Icon(Icons.wifi_tethering_error_rounded,
-              size: 50, color: Colors.lightGreen[600],),
-            SizedBox(height: 42.67),
+              size: 50, color: Color(0xFF74C13A)),
+            SizedBox(height: 36),
             Text('ネットワークエラー',
               style: TextStyle(
                 fontSize: 14,
@@ -26,38 +26,40 @@ class FeedErrorPage extends StatelessWidget {
             Text('お手数ですが電波の良い場所で',
               style: TextStyle(
                   fontSize: 12,
-                  color: Colors.grey[700]
+                  color: Color(0xFF828282),
               ),
             ),
             SizedBox(height: 6),
             Text('再度読み込みをお願いします',
               style: TextStyle(
                   fontSize: 12,
-                  color: Colors.grey[700]
+                  color: Color(0xFF828282),
               ),
             ),
-            SizedBox(height: 160),
-            SizedBox(
-              width: 327,
+            Expanded(child: Container()),
+            Container(
               height: 50,
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 24),
               child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.lightGreen[600],
-                  onPrimary: Colors.white,
-                  shape: const StadiumBorder(),
-                ),
-                onPressed: () {
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF74C13A),
+                    onPrimary: Colors.white,
+                    shape: StadiumBorder(),
+                  ),
+                  onPressed: () {
 
-                },
-                child: Text('再読み込みする',
-                  style: TextStyle(
-                    letterSpacing: 0.75,
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
+                  },
+                  child: Text('再読み込みする',
+                    style: TextStyle(
+                      letterSpacing: 0.75,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            )
+            ),
+            SizedBox(height: 32),
           ],
         ),
       ),
